ff7d027d72027aaa7b594734f5d9744f6c56806b
davetheapple <davidbarrera@u.boisestate.edu>
2015-04-28 20:18:06 -0600

    2nd commit

---
+++ www/js/index.js
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+       
+    }
+};
+
+$(document).ready(function() {
+	 var w = document.body.clientWidth;
+				var h = document.body.clientHeight;
+				// You can use either PIXI.WebGLRenderer or PIXI.CanvasRenderer
+			    var renderer = new PIXI.WebGLRenderer(w, h);
+			
+			    $('#app').append(renderer.view);
+			
+			    var stage = new PIXI.Container();
+			
+			    var bunnyTexture = PIXI.Texture.fromImage("bunny.png");
+			    var bunny = new PIXI.Sprite(bunnyTexture);
+			
+			    bunny.position.x = w/2;
+			    bunny.position.y = -h;
+			
+			    bunny.scale.x = .3;
+			    bunny.scale.y = .3;
+			
+			    stage.addChild(bunny);
+			
+			    requestAnimationFrame(animate);
+			
+			    function animate() {
+			        bunny.rotation += 0.01;
+			
+			        renderer.render(stage);
+			
+			        requestAnimationFrame(animate);
+			    }
+});
